5143cbcfba298d171630a4bb195434e48d6d29d5
CristianN01 <cristiannechifor71@gmail.com>
2024-06-25 17:44:13 +0200

    aggiunto il testo con echo nella pagina

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+    $text = 'Tony Stark, who has inherited the defense contractor Stark Industries from his late father Howard Stark, is in war-torn Afghanistan with his friend and military liaison, Lieutenant Colonel James Rhodes, to demonstrate the new "Jericho" missile. After the demonstration, his convoy is ambushed and Stark is critically wounded by a missile used by the attackers—one of his companys own.'
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +12,10 @@
     <title>php-badwords</title>
 </head>
 <body>
-    
+    <main>
+        <p>
+            <?php echo $text ?>
+        </p>
+    </main>
 </body>
 </html>
