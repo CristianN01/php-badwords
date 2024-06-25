a6c54253cbb708b5c9126b01b1dc6171a9521143
CristianN01 <cristiannechifor71@gmail.com>
2024-06-25 23:29:07 +0200

    aggiunto variabile con chiamata hget al mjio input in pagina che al momento della scrittura della parola da censurare mi stampasse dinuovo il paragrafo censurato

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-    $text = 'Tony Stark, who has inherited the defense contractor Stark Industries from his late father Howard Stark, is in war-torn Afghanistan with his friend and military liaison, Lieutenant Colonel James Rhodes, to demonstrate the new "Jericho" missile. After the demonstration, his convoy is ambushed and Stark is critically wounded by a missile used by the attackers—one of his companys own.'
+    $text = 'Tony Stark, who has inherited the defense contractor Stark Industries from his late father Howard Stark, is in war-torn Afghanistan with his friend and military liaison, Lieutenant Colonel James Rhodes, to demonstrate the new "Jericho" missile. After the demonstration, his convoy is ambushed and Stark is critically wounded by a missile used by the attackers—one of his companys ow.';
+    $banword = $_GET['Bad'];
+    $replace = str_replace($banword, '***', $text);
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,8 +14,26 @@
 <body>
     <main>
         <p>
-            <?php echo $text ?>
+            <?php echo $text ?>;
         </p>
+        <h2>
+            Paragrafo composto da : <?php echo strlen($text) ?> caratteri.
+        </h2>
+
+        <form action="./index.php" method="get">
+            <input type="text" name="Bad" id="Bad">
+            <button type="submit">
+                CENSURA
+            </button>
+        </form>
+
+        <p>
+            <?php echo $replace ?>;
+        </p>
+
+        <h2>
+            Paragrafo censurato composto da : <?php echo strlen($replace) ?> caratteri.
+        </h2>
     </main>
 </body>
 </html>
